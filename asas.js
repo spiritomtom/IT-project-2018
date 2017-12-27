789fcfb8b50a42020e2f99ee8f589e9cc6f04b7d
SkyDoge69 <luc2002penta@gmail.com>
2017-12-27 20:24:43 +0200

    Update asas.js

--- asas.js
+++ asas.js
@@ -6,7 +6,7 @@ document.getElementById("btn").innerHTML = "So, how would you like to die?";
 
                                 /*function choice*/
 function myFunction() {
-    var y = document.getElementById("mySelect").value;
+    var y = document.getElementById("select").value;
     document.getElementById("result").innerHTML = y ;
 }
 
@@ -21,4 +21,18 @@ function bigImg(x) {
 function normalImg(x) {
     x.style.height = "250px";
     x.style.width = "250px";
+}
+
+function one() {
+    var newButton = '<select id="select" onchange="myFunction()">Ma nema jeff<option value ="">▼ ▼ ▼ ▼ ▼ ▼ <option value="That is the most requested one , you are boring.">By a headshot<option value="That is my favorite, I respekt you , gentlemen.">Throat slitting<option value="Quite cruel , I can respekt that.">Bone cracking<option value="You enjoy hurting yourself , dont you?">Burnt alive</select>'; 
+
+    
+
+    document.getElementById("a").innerHTML =  newButton;
+
+}
+
+function main() {
+    one();
+    change();
 }
